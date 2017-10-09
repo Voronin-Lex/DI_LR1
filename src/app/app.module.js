5cafecdf09ade17a4fc219b36e3723dc414d91ce
alex8 <alexus2074@gmail.com>
2017-10-09 19:55:33 +0300

    add frame of project

---
+++ src/app/app.module.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Zver on 09.10.2017.
+ */
---
+++ src/framework/index.js
@@ -0,0 +1,9 @@
+import {Module as WFMModel} from './core/module'
+import {Component as WFMComponent} from './core/component'
+import {bootstrap} from './core/bootstrap'
+
+export {
+    WFMModel,
+    WFMComponent,
+    bootstrap
+}
